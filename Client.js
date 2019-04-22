468846418646f556202a964f7386aee4071f4e01
George Argyrousis <g.argyrousis@student.rug.nl>
2019-04-22 21:29:41 +0200

    Added classes to Client and Server

--- Client.js
+++ Client.js
@@ -3,61 +3,71 @@
 const net = require('net');
 const {workFunction, settings} = require('./Setup');
 
-const client = new net.Socket();
-let interval = null;
+class Client {
 
-/**
- * Connect to predefined host and port;
- */
-const connect = () => {
-    client.connect({port: settings.port});
-}
+    constructor() {
+        this.client = new net.Socket();
+        this.state = {
+            interval: null,
+        }
+        /**
+         * Action responder uppon a connection event.
+         * Will only get triggered after a succesfull
+         * invokation of the connect function;
+         */
+        this.client.on('connect', () => {
+            if (this.state.interval !== null) {
+                clearInterval(this.state.interval);
+                this.state.interval = null;
+            }
+        });
 
-/**
- * Set an interval that continuously tries
- * to connect to the origin server;
- */
-const tryToConnect = () => {
-    if (settings.debug) console.log(interval === null ? "Setting Interval" : "Interval Set");
-    if(interval === null) {
-        interval = setInterval(connect, 5000);
+        /**
+         * Action responder uppon being invoked
+         * from the server;
+         */
+        this.client.on('data', (response) => {
+            const jsonResponse = JSON.parse(response);
+            const { id, load } = jsonResponse;
+            if (settings.debug) console.log(`${id} :: Received Load - ${load}`);
+            const data = workFunction(jsonResponse);
+            const jsonData = JSON.stringify({
+                id,
+                load,
+                data,
+            });
+            this.client.write(jsonData);
+        });
+
+        /**
+         * Action responder when an error is being thrown;
+         */
+        this.client.on('error', () => {
+            this.tryToConnect();
+        });
+
+        this.connect();
+    }
+
+    /**
+     * Connect to predefined host and port;
+     */
+    connect() {
+        this.client.connect({ 
+            port: settings.port,
+        });
+    }
+
+    /**
+     * Set an interval that continuously tries
+     * to connect to the origin server;
+     */
+    tryToConnect() {
+        if (settings.debug) console.log(this.state.interval === null ? "Setting Interval" : "Interval Set");
+        if (this.state.interval === null) {
+            this.state.interval = setInterval(() => this.connect(), 5000);
+        }
     }
 }
 
-/**
- * Action responder uppon a connection event.
- * Will only get triggered after a succesfull
- * invokation of the connect function;
- */
-client.on('connect', () => {
-    if (interval !== null) {
-        clearInterval(interval);
-        interval = null;
-    }
-});
-
-/**
- * Action responder uppon being invoked
- * from the server;
- */
-client.on('data', (response) => {
-    const jsonResponse = JSON.parse(response);
-    const { id, load } = jsonResponse;
-    if(settings.debug) console.log(`${id} :: Received Load - ${load}`);
-    const data = workFunction(jsonResponse);
-    const jsonData = JSON.stringify({
-        id,
-        load,
-        data,
-    });
-    client.write(jsonData);
-});
-
-/**
- * Action responder when an error is being thrown;
- */
-client.on('error', () => {
-    tryToConnect();
-});
-
-connect();
+new Client();
--- Server.js
+++ Server.js
@@ -2,48 +2,61 @@
 
 const net = require('net');
 const { settings, stepFunction } = require('./Setup');
+ 
+class Server {
 
-const clientData = {};
-let id = -1;
-
-/**
- * Create a server and listen for Incomming connections;
- */
-const server = net.createServer((connection) => {
-    //Create a unique id - Math.floor(Math.random() * 1e10).toString();
-    id++;
-    const data = {
-        id,
-        load: stepFunction(),
-    };    
-
-    /**
-     * Action responder uppon the client finishing
-     * one step;
-     */
-    connection.on('data', (response) => {
-        const {id, load, data} = JSON.parse(response);
-        clientData[id] = {
-            id,
-            load: stepFunction(),
-            data,
+    constructor() {
+        this.state = {
+            clientData: {},
+            id: -1,
         };
-        const jsonData = JSON.stringify(clientData[id]);
-        if (settings.debug) console.log(`${id} :: [${load}] - ${data}`);
-        connection.write(jsonData);
-    });
+        this.startServer();
+    }
 
     /**
-     * Remove the connection when the client dies;
+     * Create a server and listen for Incomming connections;
      */
-    connection.on('end', () => {
-        delete clientData[id];
-    });
+    startServer() {
+        const server = net.createServer((connection) => {
+            //Create a unique id - Math.floor(Math.random() * 1e10).toString();
+            this.state.id++;
+            const id = this.state.id;
+            
+            const data = {
+                id,
+                load: stepFunction(),
+            };
 
-    connection.write(JSON.stringify(data));
-});
+            /**
+             * Action responder uppon the client finishing
+             * one step;
+             */
+            connection.on('data', (response) => {
+                const { id, load, data } = JSON.parse(response);
+                this.state.clientData[id] = {
+                    id,
+                    load: stepFunction(),
+                    data,
+                };
+                const jsonData = JSON.stringify(this.state.clientData[id]);
+                if (settings.debug) console.log(`${id} :: [${load}] - ${data}`);
+                connection.write(jsonData);
+            });
 
-server.listen(settings.port, () => {
-    console.log('Listening...');
-});
+            /**
+             * Remove the connection when the client dies;
+             */
+            connection.on('end', () => {
+                delete this.state.clientData[id];
+            });
 
+            connection.write(JSON.stringify(data));
+        });
+
+        server.listen(settings.port, () => {
+            console.log('Listening...');
+        });
+    }
+}
+
+new Server();
