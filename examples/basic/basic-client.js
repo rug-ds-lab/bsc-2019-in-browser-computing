5041ba338cdc00c0ebe11ae76d1f1cdbebe01f80
Tolga Parlan <tolgaparlan@gmail.com>
2019-06-08 15:22:04 +0200

    modified the prime example to use a webworker file

--- examples/basic/basic-client.js
+++ examples/basic/basic-client.js
@@ -1,19 +1,5 @@
-const findPrimeFactors = (num) => {
-    let factors = [];
-
-    for (let i = 2; i <= num; i++) {
-        while ((num % i) === 0) {
-            factors.push(i);
-            num /= i;
-        }
-    }
-
-    return factors;
-};
-
 new Client({
-    host: "http://localhost",
-    port: 3000,
+    socket: io("localhost:3000"),
     debug: true,
-    workFunction: findPrimeFactors,
+    workFile: "/examples/basic/work.js",
 });
