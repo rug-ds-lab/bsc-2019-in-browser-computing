3e6dc1f1738c661d61e337b0c97d62f849402419
George Argyrousis <g.argyrousis@student.rug.nl>
2019-07-08 18:50:05 +0200

    Added test for single and chunk distribution types

--- src/tests/Test.js
+++ src/tests/Test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const LoadBalancer = require('../server/loadDistribution/LoadBalancer.js');
-const {assert, nextInt} = require('../Utilities.js');
+const LoadBalancer = require('../server/LoadBalancer.js');
+const { assert, nextInt } = require('../Utilities.js');
 
 const generateRandomClients = (seed=10) => {
     const types = ['single', 'chunk', 'adjustable'];
@@ -11,7 +11,6 @@ const generateRandomClients = (seed=10) => {
         clients[i] = {
             load: nextInt(1, 300),
             loadType,
-            //miliseconds
             responseTime: nextInt(400, 3 * 60000),
             rating: 0,
         };
@@ -20,53 +19,59 @@ const generateRandomClients = (seed=10) => {
     return clients;
 }
 
-const testLoadBalancerSingle = () => {
-    const loadBalancer = new LoadBalancer({ test: {} }, {
-        type: 'single'
-    }, [1, 2, 3, 4, 5, 6, 7]);
+const testLoadBalancerSingleChunk = () => {
+    const types = [
+        {
+            type: 'single',
+            size: 1,
+        },
+        {
+            type: 'chunk',
+            size: 400,
+        },
+        {
+            type: 'chunk',
+            size: 1,
+        },
+        {
+            type: 'chunk',
+            size: 10000,
+        },
+        {
+            type: 'single',
+            size: -1,
+        },
+    ];
 
-    [1, 2, 3, 4, 5, 6, 7].forEach((item) => {
-        assert(loadBalancer.getDistributionTask() === item);
-    });
-    assert(loadBalancer.getDistributionTask().length === [].length);
-}
+    types.forEach((type) => {
+        const loadBalancer = new LoadBalancer(null, type);
 
-const testLoadBalancerChunk = () => {
-    const loadBalancer = new LoadBalancer({test: {}}, {
-        type: 'chunck',
-        size: 2,
-    }, [1, 2, 3, 4, 5, 6, 7]);
-
-    const testLoadBalancer = {
-        test1: [1, 2],
-        test2: [3, 4],
-        test3: [5, 6],
-        test4: [7],
-        test5: [],
-        test6: [],
-    };
-    Object.keys(testLoadBalancer).forEach((key) => {
-        assert(loadBalancer.getDistributionTask().toString() === testLoadBalancer[key].toString());
+        const size = loadBalancer.getTaskSize();
+        
+        if (type.type === 'single') {
+            assert(size === 1);
+        } else {
+            assert(size === type.size);
+        }
     });
 }
 
 const testLoadBalancerAdaptive = () => {
-    const clients = generateRandomClients();
-    const steps = [];
-    for(let i = 0; i < 10000; i++) steps.push(i);
+    // const clients = generateRandomClients();
+    // const steps = [];
+    // for(let i = 0; i < 10000; i++) steps.push(i);
 
-    const loadBalancer = new LoadBalancer(clients, {
-        type: 'adaptive',
-    }, steps);
+    // const loadBalancer = new LoadBalancer(clients, {
+    //     type: 'adaptive',
+    // }, steps);
 
-    Object.keys(clients).forEach((id) => {
-        loadBalancer.getDistributionTask(id);
-    });
+    // Object.keys(clients).forEach((id) => {
+    //     loadBalancer.getDistributionTask(id);
+    // });
 }
 
 const tests = {
-    testLoadBalancerSingle,
-    testLoadBalancerChunk,
+    testLoadBalancerSingleChunk,
     testLoadBalancerAdaptive,
 };
 Object.keys(tests).forEach((test, i) => {
